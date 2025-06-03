29e80366a6de2ad01aa5bd22c5c802f4bc27a01e
Humaun Kabir <Humaunkabir5561@gmail.com>
2025-06-04 00:15:32 +0600

    আপনর বরত

--- CLASS-3 Array & Array Function/loop.php
+++ CLASS-3 Array & Array Function/loop.php
@@ -1,5 +1,5 @@
 <?php
-$number = array(4,6,4,87,5,8,4,8 );
+$numbers = array(4,6,4,87,5,8,4,8 );
 $countrys  = [
 "name" =>" Humaun kabir",
 "roll" => 1,
@@ -9,9 +9,23 @@ $countrys  = [
 
 
 
-$numberOfItems = count($number);
+foreach($numbers as $number){
+    echo $number;
+}
 
-foreach ($number as $number){
+
+$lenth = count($numbers);
+
+for($i = 0; $i<$lenth; $i++){
+    echo $numbers[$i];
+
+}
+
+
+
+$numberOfItems = count($numbers);
+
+foreach ($numbers as $number){
     echo $number ;
 }
 
